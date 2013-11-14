cc226910287aad6f55019e4a4b13d267bc17592c
Simoniz <simonblanchette4@gmail.com>
2013-11-14 14:04:23 -0500

    implémentation getTransposee

--- Matrice.java
+++ Matrice.java
@@ -1,4 +1,3 @@
-
 class Matrice {
 	private double[][] matrice;
 	
@@ -9,12 +8,12 @@ class Matrice {
 	
 	public double getElement(int ligne, int colonne)
 	{
-		return matrice[ligne][colonne];
+		return this.matrice[ligne][colonne];
 	}
 	
 	public void setElement(int ligne, int colonne, double element)
 	{
-		matrice[ligne][colonne] = element;
+		this.matrice[ligne][colonne] = element;
 	}
 	
 	public int getLignes()
@@ -29,16 +28,17 @@ class Matrice {
 	
 	public Matrice additionner(Matrice matrice)
 	{
-		if (memeFormat(matrice)){
-			// TODO : throw exception Ã  venir
-		}
-		
 		double[][] elements = new double[getLignes()][getColonnes()];
 		
-		for (int ligne = 0;ligne < getLignes();ligne++) {
-			for (int colonne = 0; colonne < getColonnes();colonne++) {
-				elements[ligne][colonne] = getElement(ligne, colonne) + matrice.getElement(ligne, colonne);
+		if (memeFormat(matrice)){
+			for (int ligne = 0;ligne < getLignes();ligne++) {
+				for (int colonne = 0; colonne < getColonnes();colonne++) {
+					elements[ligne][colonne] = getElement(ligne, colonne) + matrice.getElement(ligne, colonne);
+				}
 			}
+		}
+		else {
+			System.out.println("Addition impossible, les matrices sont de format différent");
 		}
 		
 		return new Matrice(elements);
@@ -59,30 +59,107 @@ class Matrice {
 	
 	public Matrice faireProduitMatriciel(Matrice matrice)
 	{
-		double[][] elements = new double[getLignes()][getColonnes()];
+		double[][] elements = new double[getLignes()][matrice.getColonnes()];
 		
+		/* Vérifier si le nombre de colonne de la matrice actuelle
+		 * est égal au nombre de ligne de la matrice à multiplier */
+		if (memeLigneColonne(matrice)) 
+			// On parcours chaques lignes de la matrice actuelle 
+			for (int ligne = 0; ligne < getLignes(); ligne++) 
+				// On parcours chaque colonne de la matrice à multiplier
+				for (int colonneB = 0; colonneB < matrice.getColonnes(); colonneB++) 
+					// On parcours chaque colonne de la matrice actuelle
+					for (int colonneA = 0; colonneA < getColonnes(); colonneA++) 
+						 elements[ligne][colonneB] += getElement(ligne,colonneA) * matrice.getElement(colonneA, colonneB);
+		else
+			System.out.println("Produit impossible!, Le nombre de ligne de la matrice A doit " +
+					"être égale au nombre de colonnne de la matrice B");
 		
-		return this;
+		return new Matrice(elements);
 	}
 	
 	public double getTrace()
 	{
-		return 1;
+		// Si la matrice n'est pas carré, on retourne 0
+		if (!estCarre()) {
+			System.out.println("Trace impossible, la matrice doit être carré!");
+			return 0;
+		}
+		
+		double trace = 0;
+		
+		for (int ligne=0; ligne < getLignes(); ligne++) {
+			for (int colonne=0; colonne < getColonnes(); colonne++) {
+				if (ligne==colonne) {
+					// On additionne la diagonale
+					trace += getElement(ligne,colonne);
+				}
+			}
+		}
+		return trace;
 	}
 	
 	public double getDeterminant()
 	{
-		return 1;
+		// Si la matrice n'est pas carré, on retourne 0
+		if (!estCarre()) {
+			System.out.println("Déterminant impossible, la matrice doit être carré!");
+			return 0;
+		}
+			
+		int nbLignes = getLignes();
+		double determinant = 0;
+		
+		switch (nbLignes)
+		{
+			case 1: determinant = getElement(0,0); break;
+			default: determinant = getDetOrdreN(); break; 
+		}
+		
+		return determinant;
 	}
 	
 	public Matrice getTransposee()
 	{
-		return this;
+		Matrice mat = new Matrice(new double[getLignes()][getColonnes()]);
+		
+		// Si la matrice n'est pas carré, on retourne une matrice vide
+		if (!estCarre()) {
+			System.out.println("Transposée impossible, " +
+					"la matrice n'est pas carrée!");
+			return mat;
+		}
+		
+		for (int ligne = 0; ligne < getLignes(); ligne++)
+			for (int colonne = 0; colonne < getColonnes(); colonne++) 
+				mat.setElement(colonne, ligne, getElement(ligne,colonne));
+		
+		return mat;
 	}
 	
 	public Matrice getCoMatrice()
 	{
-		return this;
+		Matrice mat = new Matrice(new double[getLignes()][getColonnes()]);
+		double determinant = 0;
+		boolean estPair = false;
+		
+		if (!estCarre()) {
+			System.out.println("Comatrice impossible, la matrice doit être carre.");
+			return mat;
+		}
+		
+		for (int ligne = 0; ligne < getLignes(); ligne++)
+			for (int colonne = 0; colonne < getColonnes(); colonne++){
+				estPair = estPair(ligne+colonne);
+				determinant = getComplementAlgebrique(ligne, colonne);
+				
+				if ( (estPair && determinant < 0) || (!estPair && determinant > 0))
+					determinant *= -1;
+				
+				mat.setElement(ligne, colonne, determinant);
+			}
+		
+		return mat;
 	}
 	
 	public Matrice getMatriceInverse()
@@ -92,7 +169,10 @@ class Matrice {
 	
 	public boolean estCarre()
 	{
-		return true;
+		if (getLignes() == getColonnes())
+			return true;
+		else
+			return false;
 	}
 	
 	public boolean estTriangulaire()
@@ -105,21 +185,96 @@ class Matrice {
 		return true;
 	}
 	
-	public boolean memeFormat(Matrice matrice)
+	private boolean memeFormat(Matrice matrice)
 	{
-		if (this.getLignes() == matrice.getLignes() && this.getColonnes() == matrice.getColonnes())	{
+		if (this.getLignes() == matrice.getLignes() && this.getColonnes() == matrice.getColonnes())
 			return true;
-		}
 		
 		return false;
 	}
 	
-	public boolean memeLigneColonne(Matrice matrice) 
+	private boolean memeLigneColonne(Matrice matrice) 
 	{
-		if (this.getLignes() == matrice.getColonnes() && this.getColonnes() == matrice.getLignes()) {
+		if (this.getColonnes() == matrice.getLignes())
 			return true;
-		}
 		
 		return false;
+	}
+	
+	public String afficherMatrice()
+	{
+		String output = "";
+		for (int ligne=0; ligne < getLignes(); ligne++) {
+			
+			output += "| ";
+			
+			for (int colonne=0; colonne < getColonnes(); colonne++) {
+				output += this.matrice[ligne][colonne];
+				output += " ";
+			}
+			
+			output += "|"+"\n";
+		}
+		
+		return output;
+	}
+	
+	private double getDetOrdre2()
+	{		
+		return getElement(0,0)*getElement(1,1) 
+				- getElement(1,0)*getElement(0,1); 
+	}
+	
+	private double getDetOrdreN()
+	{
+		if (getLignes() == 2 && getColonnes() == 2)
+			return getDetOrdre2();
+		
+		double determinant = 0, detLigne = 0;
+		
+		for (int colonne=0; colonne < getColonnes(); colonne++) {	
+			detLigne = getElement(0, colonne) * getComplementAlgebrique(0, colonne);
+			
+			if (!estPair(colonne)) 
+				detLigne *= -1;
+			
+			determinant += detLigne;
+		}
+		
+		return determinant;
+	}
+	
+	private double getComplementAlgebrique(int ln, int col)
+	{
+		int posNewLigne   = 0;
+		int posNewColonne = 0;
+		boolean isNewLine = false;
+		
+		Matrice newMat = new Matrice(new double[getLignes() -1][getColonnes() -1]);
+		
+		for (int ligne=0; ligne < getLignes(); ligne++) {
+  			isNewLine = false;
+			for (int colonne=0; colonne < getColonnes(); colonne++) {
+ 				if (ligne != ln && colonne != col) {
+					newMat.setElement(posNewLigne, posNewColonne++, getElement(ligne, colonne));
+					isNewLine = true;
+				}
+			}
+			posNewColonne = 0;
+			
+ 			if (isNewLine)
+				posNewLigne++;
+		}
+		
+		return newMat.getDetOrdreN();
+	}
+	
+	
+	private boolean estPair(int nbr)
+	{
+		if (nbr%2 == 0)
+			return true;
+		else
+			return false;
 	}
 }
--- test.java
+++ test.java
@@ -9,20 +9,28 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double[][] arrA = {{2,4},{5,7},{8,9}};
-		double[][] arrB = {{1,7},{3,4}};
-		double[][] arrC = {{2,2},{2,2},{2,2}};
+		double[][] arrA = {{7,2,3},{-1,0,-4}};
+		double[][] arrB = {{2,4},{-5,1},{-1,0}};
+		double[][] arrC = {{2,4},{-5,1},{-1,0}};
+		double[][] arrD = {{8,6},{-6,-7}};
 		
-		double[][] arrD = {{2,4,3},{5,7,9},{8,9,4},{7,7,2}};
-		double[][] arrE = {{1,4},{2,5},{3,6}};
+		double[][] arrE = {{1,3,4},{3,5,-4},{4,7,-2}};
+		double[][] arrF = {{1,2,3},{4,5,6},{7,8,9}};
+		double[][] arrG = {{1,2,3,4,5},{6,7,8,9,10},
+				          {11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+		double[][] arrH = {{9}};
+		double[][] arrI = {{4,2,8,3},{5,1,7,5},{8,0,8,5},{3,2,3,8}};
+		double[][] arrJ = {{5,3,4},{8,1,5},{3,5,6}};
+		double[][] arrK = {{1,1,-2},{4,-1,2},{2,-6,3}};
 		
-		Matrice matriceA = new Matrice(arrD);
-		Matrice matriceB = new Matrice(arrE);
+		double[][] arrL = {{4,2,8},{5,1,7},{8,0,8}};
 		
-		Matrice matriceC = matriceA.faireProduitMatriciel(matriceB);
+		double[][] arrM = {{4,2,8,3}, {5,1,7,5}, {8,0,8,5}, {3,2,3,8}};
 		
-	    System.out.print(matriceC.afficherMatrice());
+		Matrice matA = new Matrice(arrI);
+		Matrice matB = matA.getCoMatrice(); 
 		
+		System.out.println(matA.getDeterminant());
 	}
 
 }
