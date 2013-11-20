adbef43aa34f0b573a2d9f7bd157a66ca89a6b2a
fhamelin <fredhamelin91@gmail.com>
2013-11-19 21:53:06 -0500

    Ajout de la fonction trouverXParCramer

--- Systeme.java
+++ Systeme.java
@@ -21,7 +21,35 @@ public class Systeme {
 	
 	public Matrice trouverXParCramer()
 	{
-		return matriceA;
+		double detA = matriceA.getDeterminant();
+		
+		Matrice result = new Matrice(new double[matriceB.getLignes()][matriceB.getColonnes()]);
+		
+		if (detA == 0) {
+			System.out.println("La matrice a un déterminant égal à 0.");
+			return result;
+		}
+		
+		for (int currentCol = 0; currentCol < matriceA.getColonnes();currentCol++) {
+			Matrice matriceAn = new Matrice(new double[matriceA.getLignes()][matriceA.getColonnes()]);
+			
+			for (int ligne = 0; ligne < matriceA.getLignes(); ligne++) {
+				for (int colonne = 0; colonne < matriceA.getColonnes(); colonne++) {
+					if (colonne == currentCol){
+						matriceAn.setElement(ligne, colonne, matriceB.getElement(ligne, colonne));
+					}
+					else {
+						matriceAn.setElement(ligne, colonne, matriceA.getElement(ligne, colonne));
+					}
+				}
+			}
+			
+			double detAn = matriceAn.getDeterminant();
+			
+			result.setElement(currentCol, 0, detAn / detA);
+		}
+		
+		return result;
 	}
 	
 	public Matrice trouverXParInversionMatricielle()
